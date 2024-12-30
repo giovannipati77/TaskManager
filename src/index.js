2323094a14b70d6c219ac08ee76e45da535558c2
Giovanni Patiño <giovapatino77@gmail.com>
2024-12-30 13:14:27 -0500

    Agrego variables de Entorno

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import swaggerDocs from "./swagger.js";
 import cors from "cors";
 import dotenv from "dotenv";
 
-dotenv.config({path: './src/.env'})
+dotenv.config({path: '.env'})
 const URL = process.env.MONGO_URL;
 const PORT = process.env.PORT || 3100;
 
